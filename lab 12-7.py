1e982c4397ec8abd1c7c07bc4dc43fe08e94326f
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2023-02-07 14:24:11 -0500

    Lab 12-7
    Lab 12-7

---
+++ lab 12-7.py
@@ -0,0 +1,32 @@
+#1. Create a Python file named lab_12-7.py
+#2. Create a second Python file named lab_12-7_functions.py
+#. In the lab_12-7_functions.py file, create AT LEAST 10 different
+#functions (you may copy from earlier assignments). Within the
+#functions you write, you must account for the following:
+#a. at least one function must use a dictionary (can be as the argument)
+#b. you must use at least 3 different lists
+#c. you must use a for loop, a while loop, and enumerate
+#d. you must use at least 5 built in functions (like lower()).
+##e. your functions all must return at the end.
+#f. at least 5 of the functions must be over 15 (logical) lines of code in length)
+#4. In the lab_12-7.py file, call (print) at least 4 of the functions. You
+#may choose any method to do this.
+
+
+#• Typically, imported files will contain functions and variables
+#• They will not execute code (this is done in the main file)
+#•Works in the same manner as importing modules
+from lab_12-7_function import maximumidea()
+from lab_12-7_function import minimunset()
+from lab_12-7_function import createupperstring()
+from lab_12-7_function import createlowerstring()
+
+
+
+
+#4. In the lab_12-7.py file, call (print) at least 4 of the functions. You
+#may choose any method to do this.
+print(maximumidea())
+print(minimunset())
+print(createupperstring())
+print(createlowerstring())
