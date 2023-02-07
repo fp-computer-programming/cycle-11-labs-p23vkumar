57b235d14ff98b38e1c7c95254d7ec61b7d6cba7
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2023-02-07 14:12:27 -0500

    Lab 11-1
    Lab 11-1

---
+++ Lab 11-1.py
@@ -0,0 +1,24 @@
+#1. Create a Python file named lab_11-1.py
+#2. Create a dictionary called row.
+#3. Add key:value pairs to the dictionary for each person in the row,
+#where the key is the computer they are sitting at and the value is
+#their first name.
+#4. Print the dictionary and notice the order the key:value pairs are
+#listed in.
+#5. Imagine you were out for a day and someone else sat at your
+#computer. How would you update the dictionary to reflect this
+#change? Add this line after your print statement.
+
+
+#dicitionary called row having a set of keys and values pertaining to students in the class
+row = {'Tanner Langis':2, 'Jack Rydzik':3, 'Preston Lyons':5, 'Vishnu Kumar':7, 'Brian Connolly':8, 'Richie Whitecomb':10, 'Ben Ackerman':13, 'Aiden Elliff':15, 'Nick Orellana':17, 'Nathanial Fabricatore':18, 'Colin Sasser':21, 'Joe Marotta':24, 'Jan Salafia':27, 'Jack Mchale':28, 'Will Clegg':30, 'Sean Salafia':34, 'Robbie Donohue':25, 'Ethan Forrest':26}
+#printed the dictionary, row
+print(row)
+
+
+#5. Imagine you were out for a day and someone else sat at your
+#computer. How would you update the dictionary to reflect this
+#change? Add this line after your print statement.
+
+
+print('If I was out for a day and someone else sat at my computer, I would update the dictionary to reflect this change by calling the key within the dictionary and making += to the desired value to be updated, similar to thagt of lists.')
