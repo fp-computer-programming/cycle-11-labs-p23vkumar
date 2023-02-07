b2c2d69e8d5550607066115931c0ca393d8c6450
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2023-02-07 14:23:55 -0500

    Lab 11-5
    Lab 11-5

---
+++ Lab 11-5.py
@@ -0,0 +1,20 @@
+#1. Create a Python file named lab_11-5.py
+#2. Create a list called day_date that contains the number day for the
+#next 5 days, starting with, "2", for today.
+#3. Using a tuple, assign the 5 letters from the list to a variable that
+#represents each day using a single statement. The variables can be
+#something like, thu, fri, mon, tue, wed. Look at the last slide of
+#notes for help.
+#4. Print each of the variables (thu, fri, mon, tue, wed) to see what
+#they equal
+
+
+#list called day_date that contains the number day for the
+#next 5 days, starting with, "2", for today.
+day_date = ("2", "3", "4", "5", "6",)
+(thu, fri, mon, tue, wed) = day_date
+#Using a tuple, assign the 5 letters from the list to a variable that
+#represents each day using a single statement. The variables can be
+#something like, thu, fri, mon, tue, wed. Look at the last slide of
+#notes for help.
+print(day_date)
